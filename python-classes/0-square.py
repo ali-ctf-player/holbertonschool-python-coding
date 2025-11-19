c49db3df436999fc8afd2277068115b10dad2d4c
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-19 09:46:16 +0400

    quiz commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
+    """A class that defines a square with a private size attribute"""
     def __init__(self, size):
+        """It is inside of function"""
         self.__size = size
