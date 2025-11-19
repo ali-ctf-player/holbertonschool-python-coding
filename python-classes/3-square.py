75de1c8eab5ec97a4fc4ffc34f74884f7035c70a
ali-ctf-player <eliekberbabayev23082007@gmail.com>
2025-11-19 10:17:14 +0400

    quiz commit

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""It is about class tutorial"""
+
+
+class Square:
+    """It is inside of class"""
+
+    def __init__(self, size=0):
+        """It is inside of function"""
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
